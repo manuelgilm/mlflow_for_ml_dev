eca33646e14b04208956bfadb1c8af2fc6ef337a
Manuel Gil <manuelgilsitio@gmail.com>
2023-08-19 12:59:19 -0500

    update nested runs

--- mlflow_utils.py
+++ mlflow_utils.py
@@ -27,6 +27,8 @@ def create_mlflow_experiment(experiment_name: str, artifact_location: str, tags:
         print(f"Experiment {experiment_name} already exists.")
         experiment_id = mlflow.get_experiment_by_name(experiment_name).experiment_id
 
+    mlflow.set_experiment(experiment_name=experiment_name)
+
     return experiment_id
 
 
