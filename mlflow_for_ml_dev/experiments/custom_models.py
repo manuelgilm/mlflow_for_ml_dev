36b6f51f8cc94d6b8b826fa736d2dda31b39f38f
Manuel Gil <manuelgilsitio@gmail.com>
2024-03-31 15:29:37 -0500

    adding usecase for using parameters in model signature

--- mlflow_for_ml_dev/experiments/custom_models.py
+++ mlflow_for_ml_dev/experiments/custom_models.py
@@ -128,3 +128,42 @@ class CustomClassifier(mlflow.pyfunc.PythonModel):
         )
         self.log_drift_detection(drift_results)
         return self.model.predict(model_input)
+
+
+class MultiModel(mlflow.pyfunc.PythonModel):
+    
+    def __init__(self, models: Dict[str, Any] = {}):
+        self.models = models
+
+    def fit_estimators(self, x: Union[pd.DataFrame, np.ndarray], y: Union[pd.DataFrame, np.ndarray]) -> None:
+        """
+        Fit the models to the data
+
+        :param x: The input data
+        :param y: The target data
+        """
+        for model_id, model in self.models.items():
+            print("Fitting model: ", model_id)
+            model.fit(x, y)
+
+    def predict(
+        self, context, model_input: Union[pd.DataFrame, np.ndarray], params={}
+    ) -> np.ndarray:
+        """
+        Predict the target values
+
+        :param context: The context object
+        :param model_input: The input data
+        :param params: The model parameters
+        :return: The predicted target values
+        """
+        model_id = params["model_id"]
+
+        if model_id not in self.models.keys():
+            raise ValueError(f"Model with id {model_id} not found")
+        
+        model = self.models[model_id]
+        print("Predicting with model: ", model_id)
+        prediction = model.predict(model_input)
+
+        return {model_id: prediction}
