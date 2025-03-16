445415541e82296930ce961ac48bbda4b9e5bd5a
Manuel Gil <manuelgilsitio@gmail.com>
2025-03-16 10:39:52 -0500

    Update

---
+++ examples/multi_model/entrypoint.py
@@ -0,0 +1,72 @@
+from examples.multi_model.pipeline import MultiClassifier
+from examples.multi_model.utils import get_model_signature
+from examples.multi_model.utils import get_train_test_data
+from examples.multi_model.utils import set_alias
+from examples.utils.mlflow_utils import get_or_create_experiment
+from mlflow_for_ml_dev.src.utils.folder_operations import get_project_root
+from sklearn.metrics import classification_report
+import mlflow
+
+def train_multi_model():
+    """
+    Train the multi model
+    """
+    x_train, x_test, y_train, y_test = get_train_test_data()
+
+    multi_classifier = MultiClassifier()
+    multi_classifier.train_estimators(x_train, y_train)
+
+    # get predictions
+    predictions = multi_classifier.predict(None, x_test, params={"algo": "decision_tree"})
+    
+
+    #set tracking uri
+    mlflow.set_tracking_uri(get_project_root() / "mlruns")
+
+    # set experiment
+    experiment_name = "multi_model"
+    experiment = get_or_create_experiment(
+        name=experiment_name,
+        tags={"project_name": "student_performance"},
+    )
+
+    # set the experiment description
+    mlflow.set_experiment_tag(
+        key="mlflow.note.content",
+        value="Experiment to test the random forest classifier on the student performance dataset.",
+    )
+    
+    signature = get_model_signature(x_train, y_train, {"algo": "random_forest"})
+
+    metrics_repot = classification_report(y_test, predictions, output_dict=True)
+
+    with mlflow.start_run(experiment_id=experiment.experiment_id):
+        mlflow.set_tag("model_type", "multi_model")
+        mlflow.set_tag("mlflow.note.content", "Custom model trained with random data")
+        mlflow.log_metrics(metrics_repot["weighted avg"])
+
+        mlflow.pyfunc.log_model(
+            artifact_path="multi_model",
+            python_model=multi_classifier,
+            signature=signature,
+            input_example=x_train.sample(5),
+            registered_model_name="multi_model",
+        )
+
+    set_alias()
+
+def inference_multimodel():
+    """
+    Inference the multi model
+    """
+    x_train, x_test, y_train, y_test = get_train_test_data()
+
+    client = mlflow.MlflowClient()
+    model_name = "multi_model"
+
+    #get the champion model
+    model_uri = f"models:/{model_name}@champion"
+    model = mlflow.pyfunc.load_model(model_uri)
+
+    predictions = model.predict(x_test, params={"algo": "decision_tree"})
+    print(predictions)
---
+++ examples/multi_model/utils.py
@@ -0,0 +1,72 @@
+from sklearn.datasets import make_classification
+from sklearn.model_selection import train_test_split
+import pandas as pd
+from typing import Tuple
+from typing import Dict
+import mlflow
+
+from mlflow.models import ModelSignature
+from mlflow.types.schema import Schema 
+from mlflow.types.schema import ColSpec
+from mlflow.types.schema import ParamSchema
+from mlflow.types.schema import ParamSpec
+
+
+def get_train_test_data()->Tuple[pd.DataFrame, pd.DataFrame, pd.Series, pd.Series]:
+    """
+    Get the training and testing data
+    """
+    x, y = make_classification(n_samples=1000, n_features=20, n_classes=2)
+
+    feature_columns = [f"feature_{i}" for i in range(x.shape[1])]
+    target_column = "target"
+
+    x = pd.DataFrame(x, columns=feature_columns)
+    y = pd.Series(y, name=target_column)
+                  
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+
+    return x_train, x_test, y_train, y_test
+
+def get_model_signature(input:pd.DataFrame, output:pd.Series, params:Dict[str, str])->ModelSignature:
+    """
+    Get the model signature.
+
+    :param input: The input data
+    :param output: The output data
+    :param params: The model parameters
+    :return: The model signature
+    """
+    inputs = [ColSpec(name=col, type="double") for col in input.columns]
+    output = [ColSpec(name=output.name, type="double")]
+    input_schema = Schema(inputs)
+    output_schema = Schema(output)
+    input_params = [ParamSpec(name=param, dtype="string", default=value) for param, value in params.items()]
+    param_schema = ParamSchema(input_params)
+    return ModelSignature(inputs=input_schema, outputs=output_schema, params=param_schema)
+    
+
+def set_alias():
+    """
+    Set the alias for the model
+    """
+    # search versions
+    client = mlflow.MlflowClient()
+    model_name = "multi_model"
+    model_versions = client.search_model_versions(f"name='{model_name}'")
+
+    # set alias to the latest version
+    latest_version = model_versions[0].version
+    client.set_registered_model_alias(
+        name=model_name,
+        version=latest_version,
+        alias="champion",
+    )
+
+    # set alias to the second latest version
+    second_latest_version = model_versions[1].version
+    client.set_registered_model_alias(
+        name=model_name,
+        version=second_latest_version,
+        alias="challenger",
+    )    
