92f319506b4decc24c8b0a23e55b80831af43c5f
Manuel Gil <manuelgilsitio@gmail.com>
2025-03-27 19:12:26 -0500

    updating example

--- examples/student_performance/entrypoint.py
+++ examples/student_performance/entrypoint.py
@@ -33,10 +33,6 @@ def log_sklearn_pipeline():
 
     predictions = pipeline.predict(x_test)
     print(predictions.head())
-    
-    metric_report = classification_report(
-        y_test, predictions["predictions"], output_dict=True
-    )
 
     # set tracking uri
     mlflow.set_tracking_uri(get_project_root() / "mlruns")
@@ -54,7 +50,7 @@ def log_sklearn_pipeline():
     model_signature = infer_signature(x_test, y_test)
     with mlflow.start_run(
         experiment_id=experiment.experiment_id
-    ):
+    ) as run:
         # Set description for the run
         mlflow.set_tag(
             "mlflow.note.content", "Run to test the random forest classifier."
@@ -62,20 +58,28 @@ def log_sklearn_pipeline():
         mlflow.set_tag("algo", algo)
         mlflow.set_tag("project_name", "student_performance")
         mlflow.log_params(pipeline.pipeline.get_params())
-        mlflow.log_dict(
-            dictionary=metric_report, artifact_file="classification_report.json"
-        )
-        mlflow.log_dict(
-            dictionary=metric_report, artifact_file="classification_report.yaml"
-        )
-        mlflow.log_metrics(metric_report["weighted avg"])
-                    
+         
         mlflow.sklearn.log_model(
             sk_model=pipeline.pipeline,
             artifact_path="pipeline_model",
             input_example=x_test.sample(5),
             signature=model_signature,
         )
+
+        model_uri = f"runs:/{run.info.run_id}/pipeline_model"
+        eval_data = x_test.copy()
+        eval_data["target"] = y_test
+
+        eval_result = mlflow.evaluate(
+            model = model_uri,
+            data = eval_data,
+            targets = "target",
+            model_type = "classifier",
+            evaluator_config = {
+                "metric_prefix": "eval_"
+            }
+        )
+
 
 def student_performance_inference():
     """
@@ -90,11 +94,11 @@ def student_performance_inference():
     runs = mlflow.search_runs(
         experiment_names = [experiment_name],
         filter_string=f"tags.algo = '{algo}'",
-        order_by=["metrics.f1_score desc"],
+        order_by=["metrics.eval_f1_score desc"],
 
     )
     # show the results
-    print(runs[["run_id","metrics.recall","metrics.f1-score"]].head())
+    print(runs[["run_id","metrics.eval_recall_score","metrics.eval_f1_score"]].head())
 
     # get the best run
     best_run = runs.iloc[0]
