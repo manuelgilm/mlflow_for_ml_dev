ee0ec24c3df980044fbc9dfca05e7b0a4fa74e69
Manuel Gil <manuelgilsitio@gmail.com>
2025-05-18 10:17:12 -0500

    adding local_model_serving folder

---
+++ examples/iris_classifier/inference.py
@@ -0,0 +1,21 @@
+import mlflow
+from examples.utils.decorators import mlflow_tracking_uri
+from examples.iris_classifier.data import get_train_test_data
+
+
+@mlflow_tracking_uri
+def main():
+    """
+    Main function to run the batch inference process.
+    """
+    # Load the model from the specified path
+    _, x_test, _, _ = get_train_test_data()
+    registered_model_name = "Iris_Classifier_Model"
+    model_path = f"models:/{registered_model_name}@production"
+    model = mlflow.sklearn.load_model(model_path)
+
+    # Perform inference on the test data
+    predictions = model.predict(x_test)
+    x_test["predictions"] = predictions
+    print(x_test.head())
+    print("Batch inference completed successfully.")
