5f4e00200db4ace2e8e3a22c8806938a76158f11
Manuel Gil <manuelgilsitio@gmail.com>
2025-05-25 15:38:52 -0500

    update digit_recognition folder

--- examples/digit_recognition/online_inference.py
+++ examples/digit_recognition/online_inference.py
@@ -1,4 +1,4 @@
-from examples.digit_recognition.data import get_train_test_data
+from examples.digit_recognition.data import get_train_val_test_data
 from examples.digit_recognition.data import transform_to_image
 import httpx
 import pandas as pd
@@ -9,12 +9,15 @@ import numpy as np
 def main() -> None:
     """
     Perform online inference using a REST API.
+    To deploy the model in the local server, run the following command:
+
+    `poetry run mlflow models serve -m models:/Digit_Recognition_Model@production --env-manager local`
     """
-    _, x_test, _, y_test = get_train_test_data()
+    _, _, x_test, _, _, y_test = get_train_val_test_data()
     x_test = transform_to_image(x_test)
 
-    url = "http://127.0.0.1:5000/invocations"
-    n_samples = 3
+    url = "http://127.0.0.1:5001/invocations"
+    n_samples = 5
     samples = x_test[0:n_samples]
 
     payload = {
